efc49f5454c5f08d1e0546081d9135cdec159153
AJAYrocks13 <ee210002010@iiti.ac.in>
2022-07-18 10:33:07 +0530

    2nd commit

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path , include ,re_path
+
+from . import views
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('signup',views.signup,name="signup"),
+    path('signin',views.signin,name="signin"),
+    path('signout',views.signout,name="signout"),
+  
+    path('social/', include('social.apps.django_app.urls', namespace='social')),
+    path('auth/', include('rest_framework_social_oauth2.urls')),
+]
+
