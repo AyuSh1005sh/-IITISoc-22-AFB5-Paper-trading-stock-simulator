7f756ffb4a8609917bd9cfe6a262d7bfd656d8ce
Ayush <Ayushparks1109@gmail.com>
2022-07-19 01:59:47 +0530

    midval commit

---
+++ project/views.py
@@ -0,0 +1,46 @@
+from email import message
+import django
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render(request,"authentication/index.html")
+
+def signup(request): 
+
+    if request.method == "POST":
+        username = request.POST['username']
+        Fname = request.POST['fname']
+        Lname = request.POST['Lname']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        myuser = User.objects.create_user(username,email,password)
+        myuser.first_name= Fname
+        myuser.last_name= Lname
+
+        myuser.save()
+
+        message.success(request,"your account is successfully created.")
+
+        return redirect('signin')
+   
+    
+
+
+    return render(request,"authentication/signup.html")
+
+def signin(request):
+    return render(request,"authentication/signin.html")
+
+def signout(request):
+   pass
+def support (request):
+    return HttpResponse ("Helpline Number - *********** , Email Id - PaperTrade2022@gmail.com")
+
+
